218f308c05d2d6911f26e97eb15c80814f224e2e
hymeck <noonimf@gmail.com>
2021-05-08 16:02:35 +0300

    add user login/logout

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
+/**
+ * @IsGranted("ROLE_USER")
+ */
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'user')]
-    public function index(): Response
+    #[Route('/profile', name: 'user_profile')]
+    public function profile(): Response
     {
-        return $this->render('user/index.html.twig', [
+        return $this->render('user/profile.html.twig', [
             'controller_name' => 'UserController',
         ]);
     }
